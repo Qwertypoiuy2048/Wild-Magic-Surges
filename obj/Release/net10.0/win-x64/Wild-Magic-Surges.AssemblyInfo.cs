e19c33e0cb9a0ea56e93b31e24d1e02fcf1e2043
Polyq MEEEEE <ehc.reader@gmail.com>
2026-03-31 09:22:54 -0600

    Updated, should be functional

--- obj/Release/net10.0/win-x64/Wild-Magic-Surges.AssemblyInfo.cs
+++ obj/Release/net10.0/win-x64/Wild-Magic-Surges.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wild-Magic-Surges")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029667bc06ec1c967c0419d19baca1c83fd35c8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b491349ca541be0d7e8989119f84bc624391b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wild-Magic-Surges")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wild-Magic-Surges")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
